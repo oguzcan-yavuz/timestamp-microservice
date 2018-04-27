890f887e552def8477fa5762a60b8877cf60ec9d
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-27 12:00:28 +0300

    Routes structure is changed. Now we have more modular routes.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const moment = require('moment');
-const unixTimePattern = /^\/\d+$/;
-const humanDatePattern = /^\/(January|February|March|April|May|June|July|August|September|October|November|December)%\d{4},%\d{6}$/;  // date parser?
+const unixTimePattern = /^\d+$/;
 var router = express.Router();
 
+function unixToNatural(miliseconds) {
+    return moment.unix(miliseconds).format("MMMM DD, YYYY");
+}
+
+function naturalToUnix(humanDate) {
+    return parseInt(moment.utc(humanDate).format("X"));
+}
+
+function dateMatcher(date) {
+    let results = {"unix": null, "natural": null};
+    if(unixTimePattern.test(date)) {
+        date = parseInt(date);
+        results.unix = date;
+        results.natural = unixToNatural(date);
+    }
+    else if(moment(date).isValid()) {
+        results.natural = date;
+        results.unix = naturalToUnix(date);
+    }
+    return results;
+}
+
 router.get("/", (req, res) => {
-    res.send("home!");
-});
+    res.send("Home page!");
+})
 
-router.get(unixTimePattern, (req, res) => {
-    let unixTime = parseInt(req.url.split("").splice(1).join(""));
-    let natural = moment(unixTime).format("MMMM DD, YYYY");
-    let results = {
-        unix: unixTime,
-        natural: natural
-    };
-    res.json(results);
-});
-
-router.get(humanDatePattern, (req, res) => {
-    res.send("home!");
+router.get("/:date", (req, res) => {
+    let date = req.params.date;
+    res.json(dateMatcher(date));
 });
 
 module.exports = router;
